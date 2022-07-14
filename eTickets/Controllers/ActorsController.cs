5edd94fb4485f9d280e16fb1a1108ff5f0e596e1
Haris Agic <agic@ntara.com>
2022-07-14 23:19:26 +0200

    Major Dependency-injection lifetimes

--- eTickets/Controllers/ActorsController.cs
+++ eTickets/Controllers/ActorsController.cs
@@ -17,7 +17,7 @@ namespace eTickets.Controllers
 
         public async Task<IActionResult> Index()
         {
-            var data = _service.GetAll();
+            var data = await _service.GetAll();
             return View(data);
         }
     }
